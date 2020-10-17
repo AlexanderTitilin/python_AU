9ac09a364be57384e9d4ff9d39ed96234ecee74a
ALexander <lambdadelta@gmail,com>
2020-10-17 12:00:24 +0300

    add

---
+++ leetcode/generate_md.py
@@ -0,0 +1,15 @@
+﻿source = open("source_leetcode_data.txt")
+source = source.readlines()
+name = source[0].split(". ")[1][:-1]
+link = source[1][:-1]
+name_from_link = link.split("https://leetcode.com/problems/")[1][:-1]
+code_list=[line[4:] for line in source[3:]]
+code = ''.join(code_list)
+result = f"\n## {name}\n \
+{link}\n \
+```python\n\
+{code}\n\
+```"
+file_for_result = open('intervals.md','a')
+file_for_result.write(result)
+file_for_result.close()
